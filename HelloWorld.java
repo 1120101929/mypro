6612a5206015abe2017d196695f85245c744c668
1120101929 <1061524626@qq.com>
2015-08-15 23:43:40 +0800

    edit with branch master

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,6 @@ package cn.lym.git;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello Git! This is in brh branch.");
+		System.out.println("edit with branch master");
 	}
 }
