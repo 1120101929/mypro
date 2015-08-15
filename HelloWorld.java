b2e2f22754a9de42141b201baa9a72e90f55956c
1120101929 <1061524626@qq.com>
2015-08-15 23:55:32 +0800

    add sth in branch lym

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello Git! This is in brh branch.");
 		System.out.println("edit in branch lym");
+		System.out.println("*******************");
 	}
 }
